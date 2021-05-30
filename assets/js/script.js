a5da17598ff3b454761544df0c79fc963ca13420
Jhonatan Luiz Chagas <jonatapqt01@gmail.com>
2021-05-30 18:19:04 -0300

    tema

--- assets/js/script.js
+++ assets/js/script.js
@@ -600,19 +600,6 @@ Game = {
             Game.Algoritmos.UpdateMap();
             return Game.Algoritmos.Solucao.a_estrela(origem, destino);
         },
-
-        Base64FromUrl = function (url) {
-            const data = await fetch(url);
-            const blob = await data.blob();
-            return new Promise((resolve) => {
-                const reader = new FileReader();
-                reader.readAsDataURL(blob);
-                reader.onloadend = () => {
-                    const base64data = reader.result;
-                    resolve(base64data);
-                }
-            });
-        }
     }
 }
 
